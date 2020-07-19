0969e5224eab42044f87e4b6c7a3f8ef5f428caf
Fyllipy <jfyllipy@gmail.com>
2020-07-19 16:35:52 -0300

    lol

---
+++ js/request.js
@@ -0,0 +1,14 @@
+var ourRequest = new XMLHttpRequest();
+
+    ourRequest.open('GET', 'https://raw.githubusercontent.com/Fyllipy/QuantitativosDashboard/master/quantitativos.txt');
+    ourRequest.onload = function (){
+        var ans;
+            ans = ourRequest.responseText;
+            //console.log(ans);
+            //var teste = ans[rec];
+
+            var dadosJSON = JSON.parse(ans);
+            //var recState = dadosJSON.results;
+            console.log(dadosJSON);
+    }
+    ourRequest.send();
